9e061403389fd368e9f704f5d0d783c8d3992ced
cloudrohu <cloudrohu@gmail.com>
2024-04-16 15:19:48 +0530

    header

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Setting, ContactForm, ContactMessage,FAQ,Slider,Offer
+from product.models import Category
+
 
 def index(request):
+
+    setting = Setting.objects.all().order_by('-id')[:1]
+    category = Category.objects.all()    
+    context={
+        'setting':setting,
+        'category':category
+    } 
     
-    return render(request,'main/index.html')
+    return render(request,'main/index.html',context)
